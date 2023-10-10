d8c8c639c7af2549744b01e86d4f8ca7370b5e7a
tkhs <tokuhausu@gmail.com>
2023-10-10 17:11:36 +0900

    数式の検証を追加

--- rp1/source.c
+++ rp1/source.c
@@ -45,6 +45,47 @@ void main(){
     formula[count_res] = '\0';
 
     //validate
+    int i = 0;
+    while(formula[i] != '\0'){
+        if(formula[i] == OPERATOR_PLUS || formula[i] == OPERATOR_MINUS || formula[i] == OPERATOR_MULTIPLY){
+            if((formula[i+1] < '0' && formula[i+1] > '9')&&formula[i+1] != BRACKET_OPEN){
+                printf("Invalid formula");
+                return;
+            }
+        }
+        if(formula[i] == BRACKET_CLOSE && (formula[i+1] >= '0' && formula[i+1] <= '9')){
+            printf("Invalid formula");
+            return;
+        }
+        if(formula[i] == BRACKET_OPEN){
+            int j = i+1;
+            if(formula[i-1] >= '0' && formula[i-1] <= '9'){
+                printf("Invalid formula");
+                return;
+            }
+            if(formula[j] == OPERATOR_PLUS || formula[j] == OPERATOR_MULTIPLY){
+                printf("Invalid formula");
+                return;
+            }
+            int count = 1;
+            while(formula[j] != '\0'){
+                if(formula[j] == BRACKET_OPEN){
+                    count++;
+                } else if(formula[j] == BRACKET_CLOSE){
+                    count--;
+                }
+                if(count == 0){
+                    break;
+                }
+                j++;
+            }
+            if(count != 0){
+                printf("Invalid formula");
+                return;
+            }
+        }
+        i++;
+    }
 
 
     printf("Sanitized %s", formula);
