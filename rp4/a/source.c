cb649b02550a42fa3fe9b7de32bceb258ac804b0
tkhs <tokuhausu@gmail.com>
2024-01-18 00:00:29 +0900

    ヒープサイズをコマンドライン引数から設定可能に.コードの最適化.

--- rp4/a/source.c
+++ rp4/a/source.c
@@ -4,7 +4,7 @@
 #include<time.h>
 #include "MT.h"
 
-#define MEMORY_ALLOCATION 3000000 //1MB
+#define MEMORY_ALLOCATION_DEFAULT 1000000000 //1MB
 
 typedef struct{
     long p_gen; //親の世代
@@ -13,8 +13,6 @@ typedef struct{
 } parent_and_child_gen;
 
 typedef struct{
-    long gen; //世代
-    long id; //ID
     long p_id; //親のID
 } individual;
 
@@ -23,9 +21,8 @@ typedef struct{
     long end_gen;
 } loaded_gen;
 
-//大域変数
-long num, maxGen;
-
+long MEMORY_ALLOCATION = MEMORY_ALLOCATION_DEFAULT;
+long num;
 
 /*
   娘を出産する母親を決定．
@@ -71,6 +68,8 @@ parent_and_child_gen find_nearest_child_gen(individual *individuals,loaded_gen *
         if(j >= loaded->end_gen){
             if(j - start_gen >= MEMORY_ALLOCATION / (sizeof(individual) * num)){
                 printf("Heap size is not enough!! \n");
+                printf("Set a larger heap size with the 4th command line argument or lower the value of num.\n");
+                printf("arg1 = num, arg2 = max_gen, arg3 = seed, [arg4 = max heap size(byte)]\n");
                 exit(-1);
             }
 
@@ -85,8 +84,6 @@ parent_and_child_gen find_nearest_child_gen(individual *individuals,loaded_gen *
             for (long i = j-loaded->start_gen+1; i <= loaded->end_gen - loaded->start_gen; ++i) {
                 for (long k = 0; k < num; ++k) {
                     individuals[i * num + k].p_id = nextMother();
-                    individuals[i * num + k].gen = i;
-                    individuals[i * num + k].id = k;
                 }
             }
         }
@@ -108,7 +105,7 @@ parent_and_child_gen find_nearest_child_gen(individual *individuals,loaded_gen *
     return result;
 }
 
-parent_and_child_gen find_eve(individual *individuals, loaded_gen *loaded, long *parents, long *tmp_parents,long start_gen, long num, long max_gen){
+parent_and_child_gen find_eve(individual *individuals, loaded_gen *loaded, long *parents, long *tmp_parents,long start_gen, long num){
     parent_and_child_gen result;
     result.p_gen = start_gen;
     result.p_id = 0;
@@ -160,8 +157,6 @@ void simulate(long maxGen, long num){
     for (long i = 1; i < load_num; ++i) {
         for (long j = 0; j < num; ++j) {
             individuals[i * num + j].p_id = nextMother();
-            individuals[i * num + j].gen = i;
-            individuals[i * num + j].id = j;
         }
     }
     loaded.end_gen = load_num-1;
@@ -173,7 +168,7 @@ void simulate(long maxGen, long num){
     while(1){
         res = find_nearest_child_gen(individuals, &loaded, ids, tmp_ids, res.p_gen, num, maxGen);
         if(res.c_gen != 0){
-            res = find_eve(individuals, &loaded,ids, tmp_ids, res.c_gen, num, maxGen);
+            res = find_eve(individuals, &loaded,ids, tmp_ids, res.c_gen, num);
             times++;
             print_result(times, res.p_gen, res.p_id, res.c_gen);
             res.p_gen++;
@@ -184,21 +179,24 @@ void simulate(long maxGen, long num){
     free(individuals);
     free(ids);
     free(tmp_ids);
-    printf("Eve occurrence rate is %f\n",(double) (times-1)/maxGen);
+    printf("Eve occurrence rate is %f\n",(double) times/maxGen);
 }
 
 int main(int argc, char **argv){
     clock_t t1,t2;
 
     /*******初期設定*******/
-    if(argc != 4){
-        printf("this program requires exactly three arguments\n");
-        printf("arg1 = num, arg2 = max_gen, arg3 = seed\n");
+    if(argc <= 3){
+        printf("this program requires three arguments\n");
+        printf("arg1 = num, arg2 = max_gen, arg3 = seed, [arg4 = max heap size(byte)]\n");
         exit(-1);
     }
     num = atol(argv[1]); //1世代の人口N．大域変数．
-    maxGen = atol(argv[2]); //世代数の上限G．大域変数．
+    long maxGen = atol(argv[2]); //世代数の上限G．大域変数．
     int seed = atoi(argv[3]);
+    if(argc == 5){
+        MEMORY_ALLOCATION = atol(argv[4]);
+    }
     init_genrand(seed);//乱数生成の初期化．【編集厳禁!】
     t1 = clock();
 
